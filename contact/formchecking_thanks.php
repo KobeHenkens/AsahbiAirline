787930f806869e3c4a9ea3c395588a51dc3f75a4
KobeHenkens <kobe.henkens@outlook.com>
2024-01-01 23:36:28 +0100

    update contact
    navbar toegevoegd + kleine fixes

--- contact/formchecking_thanks.php
+++ contact/formchecking_thanks.php
@@ -4,34 +4,52 @@
 
 ?><!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1,0">
-    <title>Contact - Thank you</title>
-    <link href="https://unpkg.com/@csstools/normalize.css" rel="stylesheet" />
-    <!--Icons-->
-    <script src="https://kit.fontawesome.com/1ae8808211.js" crossorigin="anonymous"></script>
-    <!--Fonts-->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <!--CSS-->
-    <link rel="stylesheet" href="../css/styles.css">
-<body>
-    <header></header>
-	<main>
-		<!--Thanks Section Start-->
-		<section class="form_sent_successfully container">
-			<div class="container-page">
-				<h1>Thank you for your submission, <?php echo htmlentities($name)?>!</h1>
-				<p>You will receive an email as soon as possible.</p>
-			</div>
-		</section>
-		<!--Thanks Section End-->
-	</main>
-	
-	<footer>
-    </footer>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Contact - Thank you</title>
+        <link rel="stylesheet" href="https://unpkg.com/@csstools/normalize.css">
+        <!--Fonts-->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,500;0,800;1,300;1,500;1,800&family=Poppins:ital,wght@0,100;0,300;0,500;0,800;1,100;1,300;1,400;1,500;1,800&display=swap" rel="stylesheet">
+        <!--Css-->
+        <link rel="stylesheet" href="../style.css">
+        <link rel="stylesheet" href="./contact.css">
+    </head>
+    <body>
+        <header>
+            <nav class="navbar">
+                <a href="../index.html"><img src="../img/Logo_airline.png" alt="logo website" class="logo"></a>
+                    <ul>
+                        <li><a href="../about/">About</a></li>
+                        <li><a href="../vakanties/">Vakanties</a></li>
+                        <li><a href="../cruises/">Cruises</a></li>
+                        <li><a href="../contact/">Contact</a></li>
+                    </ul>
+                    <div class="dropdown">
+                        <a href="#">Talen ▾</a>
+                        <div class="dropdown-content">
+                            <a href="#">Nederlands</a>
+                            <a href="#">Engels</a>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+        <main>
+            <!--Thanks Section Start-->
+            <section class="form_sent_successfully container">
+                <div class="container-page">
+                    <h1>Thank you for your submission, <?php echo htmlentities($name)?>!</h1>
+                    <p>You will receive an email as soon as possible.</p>
+                </div>
+            </section>
+            <!--Thanks Section End-->
+        </main>
+        
+        <footer>
+        </footer>
 
-</body>
+    </body>
 </html>
